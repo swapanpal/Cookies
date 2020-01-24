16204c82ab77f7d303514e6e0d706ef636f7f497
swapan <swapan.d83@gmail.com>
2020-01-24 09:27:16 +0700

    After clicking a button Image and text just change

---
+++ app/src/main/java/com/example/cookies/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.cookies;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView eatCookieImageView = findViewById(R.id.android_cookie_image_view);
+        eatCookieImageView.setImageResource(R.drawable.after_cookie);
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+        TextView statusTextView = findViewById(R.id.status_text_view);
+        statusTextView.setText("I'm so full");
+
+    }
+}
